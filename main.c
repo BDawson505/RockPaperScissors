48e4afb3caa965474656bf247c34386e0053e584
DawsonTS <bradleydawson@dawsonts.com>
2025-03-28 10:26:57 +0000

    refactor of the game function

--- main.c
+++ main.c
@@ -7,42 +7,25 @@
 
 int game(char player, char computer) 
 {
-    if (player == computer) 
-    {
-        return 0;
-    }
+    /*
+        Map the choices of the player and the computer to an integer.
+        Converting the char player or computer to an integer gives the ASCII value of the char.
+        Subtracting the ASCII value of 'a' (97) gives a value of 0 for a, 1 for b, and 2 for c.
+    */
+    int p = player - 'a';
+    int c = computer - 'a';
 
-    if (player == 'a' && computer == 'b')
-    {
-        return -1;
-    }
-
-    if (player == 'b' && computer == 'a')
-    {
-        return 1;
-    }
-
-    if (player == 'a' && computer == 'c')
-    {
-        return 1;
-    }
-
-    if (player == 'c' && computer == 'a')
-    {
-        return -1;
-    }
-
-    if (player == 'b' && computer == 'c')
-    {
-        return -1;
-    }
-
-    if (player == 'c' && computer ==  'b')
-    {
-        return 1;
-    }
-
-    return 0;
+    /*
+        Calculate the result of the game based on the player and computer choice (after converted to integers).
+        p - c: calculates the difference between the selections.
+        + 3: converts the result of the above calculation to a positive value
+        % 3: ensures the outcome is mapped to 0, 1, or 2
+          - 0: Draw 
+          - 1: Player Win
+          - 2: Computer Win
+    */
+    int calculated_result = (p - c + 3) % 3;
+    return calculated_result;
 }
 
 int main()
@@ -81,22 +64,22 @@ int main()
         %c is the format specifier for a char.
     */ 
     printf("Enter a for rock, b for paper, and c for scissors\n");
-    scanf("%c", &player);
+    scanf(" %c", &player);
 
     /*
         Call the game function and passes the player and computer selection, and stores the return value in result variable.
-        Returns -1 (computer win), 0 (draw), or 1 (player win).
+        Returns 0 (computer win), 1 (draw), or 2 (player win).
     */
     int result = game(player, computer);
 
     // Display the result to the user based on the return value of the game function.
-    if (result == -1)
+    if (result == 0)
     {
-        printf("You lost the game");
-    } else if (result == 0) {
-        printf("Game draw");
-    } else {
+        printf("Game Draw");
+    } else if (result == 1) {
         printf("You won the game");
+    } else {
+        printf("You lost");
     }
 
     // Display both player and computer choice to the user at the end of the game.
